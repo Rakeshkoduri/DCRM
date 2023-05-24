b4dc07295d20c7e98dc4806babb955c44819a70e
Rakeshkoduri <rakeshkoduri88@gmail.com>
2023-05-24 21:35:38 +0530

    registration

---
+++ dcrm/website/forms.py
@@ -0,0 +1,36 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+
+class SignupForm(UserCreationForm):
+    email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':"Email Address"}))
+    first_name =forms.CharField(label='',max_length='100',widget=forms.TextInput(attrs={'class':'form-control','placeholder':"First name"}))
+    last_name =forms.CharField(label='',max_length='100',widget=forms.TextInput(attrs={'class':'form-control','placeholder':"Last name"}))
+
+
+    class Meta:
+        model = User
+        fields = ('username','first_name','last_name','password1','password2')
+
+
+
+    def __init__(self, * args, ** kwargs):
+        super().__init__(* args, ** kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'forms-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'User Name'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = "<span class='form-text text-muted'> <small>Required .150 character or fewer Letters,digits and @/./+/-/_only.</small></span>"
+
+
+        self.fields['password1'].widget.attrs['class'] = 'forms-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password1'].label=''
+        self.fields['password1'].help_text = "<ul class='form-text text-muted small'><li>Your password can\'t be too other personal information.</li>"
+
+
+        self.fields['password2'].widget.attrs['class'] = 'forms-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"> <small> Enter the same password as before for verification .</small><span>'
+
